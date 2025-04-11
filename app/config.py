f45c2a64a07e0a5e3e65525af12d24880410c73e
Rohit Banka <banka.rohit1809@gmail.com>
2025-04-11 15:07:44 +0530

    updating required env variables

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 def load_env_variables():
     load_dotenv()
 
-    required_vars = ["FYERS_APP_ID", "FYERS_SECRET_ID", "FYERS_REDIRECT_URI", "FYERS_AUTH_CODE", "WEBHOOK_SECRET_TOKEN"]
+    required_vars = ["FYERS_APP_ID", "FYERS_SECRET_ID", "FYERS_REDIRECT_URI", "WEBHOOK_SECRET_TOKEN","GOOGLE_SHEET_ID"]
     for var in required_vars:
         if not os.getenv(var):
             raise EnvironmentError(f"Missing required environment variable: {var}")
