398f402c4cc0952815ed11ea980db4230df57481
Rohit Banka <banka.rohit1809@gmail.com>
2025-04-15 06:47:09 +0530

    add fyers_pin into required os params

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 def load_env_variables():
     load_dotenv()
 
-    required_vars = ["FYERS_APP_ID", "FYERS_SECRET_ID", "FYERS_REDIRECT_URI", "WEBHOOK_SECRET_TOKEN","GOOGLE_SHEET_ID"]
+    required_vars = ["FYERS_APP_ID", "FYERS_SECRET_ID", "FYERS_REDIRECT_URI", "WEBHOOK_SECRET_TOKEN","GOOGLE_SHEET_ID","FYERS_PIN"]
     for var in required_vars:
         if not os.getenv(var):
             raise EnvironmentError(f"Missing required environment variable: {var}")
