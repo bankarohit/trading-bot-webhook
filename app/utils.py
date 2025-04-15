ce6fb97ef05bb36c036fe22593e50fdb62fc8b31
Rohit Banka <banka.rohit1809@gmail.com>
2025-04-15 19:22:04 +0530

    remove some print statements

--- app/utils.py
+++ app/utils.py
@@ -25,32 +25,19 @@ symbol_master_columns = [
 def get_symbol_from_csv(symbol, strike_price, option_type, expiry_type):
     try:
         print(f"[DEBUG] Requested: symbol={symbol.upper()}, strike_price={round(float(strike_price))}, option_type={option_type.upper()}, expiry_type={expiry_type}")
-
         df = pd.read_csv(symbol_master_url, header=None, names=symbol_master_columns)
-
         df = df[df['underlying_symbol'].str.upper() == symbol.upper()]
-
-
         df = df[df['strike_price'].astype(float).round() == round(float(strike_price))]
-
-
         df = df[df['option_type'].str.upper() == option_type.upper()]
-
         today = pd.Timestamp.now().normalize()
-
-        
         df['expiry_date'] = pd.to_datetime(df['expiry_date'], unit='s', errors='coerce')
         df = df.dropna(subset=['expiry_date'])
-        
         df = df[df['expiry_date'].dt.normalize() >= today]
-
-
         expiry = None
         if expiry_type.upper() == "WEEKLY":
             df = df.sort_values('expiry_date')
             expiry = df.iloc[0]['expiry_date'] if not df.empty else None
             print(f"[DEBUG] Chosen weekly expiry: {expiry}")
-
         elif expiry_type.upper() == "MONTHLY":
             # Match symbols like NIFTY25APR, BANKNIFTY26JAN etc.
             monthly_pattern = re.compile(rf"{symbol.upper()}\d{{2}}[A-Z]{{3}}")
@@ -59,16 +46,11 @@ def get_symbol_from_csv(symbol, strike_price, option_type, expiry_type):
             expiry = df.iloc[0]['expiry_date'] if not df.empty else None
             print(f"[DEBUG] Chosen monthly expiry: {expiry}")
         else:
-            print(f"[ERROR] Invalid expiry_type: {expiry_type}")
             return None
-
         result = df[df['expiry_date'] == expiry]
-        print(f"[DEBUG] Final match count: {len(result)}")
-        print("[DEBUG] Final DataFrame:")
-        print(df[['underlying_symbol','symbol_ticker', 'strike_price', 'option_type', 'expiry_date']].to_string(index=False))
-
-        return result.iloc[0]['symbol_ticker'] if not result.empty else None
-
+        fyersTickerSymbol = result.iloc[0]['symbol_ticker'] if not result.empty else None
+        print(f"FyersTickerSYmbol: {fyersTickerSymbol}")
+        return fyersTickerSymbol
     except Exception as e:
         print(f"[ERROR] Exception in get_symbol_from_csv: {str(e)}")
         return None
